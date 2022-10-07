7b1748e68b521dc9499fad988f26826dc5547cef
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-07 08:30:30 +0700

    hi

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/image/logo.png" />
+          <meta name="theme-color" content="#fff" />
+          <link rel="shortcut icon" href="/image/logo.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
