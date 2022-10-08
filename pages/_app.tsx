8ee49f3ce39cabfb208c8505390f2bc7c140bf15
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-08 17:05:44 +0700

    add backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,38 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
+import { Session } from 'next-auth';
+import { withTRPC } from '@trpc/next';
+import { SessionProvider } from 'next-auth/react';
+import { ServerRouter } from '../server/router';
 
-export default MyApp;
+const App = ({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
+};
+
+export default withTRPC<ServerRouter>({
+  config({ ctx }) {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
+
+    return {
+      url,
+      headers: {
+        'x-ssr': '1',
+      },
+    };
+  },
+  ssr: true,
+})(App);
