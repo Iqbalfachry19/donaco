164ae5061cd683050af969316666a48542e59d1a
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-12 11:22:02 +0700

    notif

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,13 +9,23 @@ import {
   IMessage,
 } from '@novu/notification-center';
 import { useRouter } from 'next/router';
-
+import type { AddRequest } from '../pages/api/task/add';
 type Props = {};
 
 const Header = (props: Props) => {
   const { data } = useSession();
   const router = useRouter();
   function onNotificationClick(notification: IMessage) {}
+  async function createTask(req: AddRequest): Promise<void> {
+    const res = await fetch('/api/task/add', {
+      method: 'POST',
+      body: JSON.stringify(req),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log(res);
+  }
   return (
     <header className="bg-[#00aeef] flex items-center text-white justify-between">
       <div className="flex items-center px-2 py-2 space-x-2">
@@ -37,6 +47,17 @@ const Header = (props: Props) => {
         </Link>
       </nav>
       <div className="flex space-x-2 px-2 items-center">
+        <p
+          onClick={() =>
+            createTask({
+              userId: '63395cf0ee1316a5c8b25873',
+              x: 2,
+              y: 4,
+            })
+          }
+        >
+          2 + 4
+        </p>
         <NovuProvider
           subscriberId={'63395cf0ee1316a5c8b25873'}
           applicationIdentifier={'Kb-zKM23Fwbf'}
