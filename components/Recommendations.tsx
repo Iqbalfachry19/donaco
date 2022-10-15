c3e18693e2a3f7b55aecbb317e5838db27f5be1a
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-15 10:52:22 +0700

    hi

--- components/Recommendations.tsx
+++ components/Recommendations.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 type Props = {};
@@ -11,7 +12,7 @@ const data = [
   {
     id: '2',
     imageUrl: '/image/banjir.jpeg',
-    title: 'Ribuan warga terdampak banjir di kalteng',
+    title: 'Banjir di pekanbaru',
   },
 ];
 const dataJenis = [
@@ -27,12 +28,28 @@ const dataJenis = [
   },
 ];
 const Recommendations = (props: Props) => {
+  const router = useRouter();
   return (
     <div className="max-w-4xl mx-auto p-2">
       <h1>Recommendations</h1>
       <div className="flex space-x-2  overflow-x-scroll lg:overflow-hidden">
         {data.map((recommendation) => (
-          <div key={recommendation.id} className="cursor-pointer">
+          <div
+            key={recommendation.id}
+            onClick={() =>
+              router.push(
+                {
+                  pathname: `/donasi/${recommendation.id}`,
+                  query: {
+                    imageUrl: recommendation.imageUrl,
+                    title: recommendation.title,
+                  },
+                },
+                `/donasi/${recommendation.id}`,
+              )
+            }
+            className="cursor-pointer"
+          >
             <div className="w-80 h-40 relative rounded-md  overflow-hidden">
               <Image src={recommendation.imageUrl} layout="fill" alt="" />
             </div>
