1a08737d9fc2d7b8d57476a70970eb2fc6d40e94
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-15 10:52:30 +0700

    hi

---
+++ pages/donasi/[id].tsx
@@ -0,0 +1,29 @@
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Image from 'next/image';
+type Props = {};
+
+const DetailDonasi = (props: Props) => {
+  const router = useRouter();
+
+  const { title, imageUrl } = router.query;
+  return (
+    <div className="font-body">
+      <Head>
+        <title>Donaco - {title}</title>
+        <meta name="description" content="donaco is web for donating" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="max-w-4xl mx-auto p-2 ">
+        <div className="flex flex-col cursor-pointer items-center justify-center">
+          <div className="w-80 h-40 relative rounded-md  overflow-hidden">
+            <Image src={imageUrl} layout="fill" alt="" />
+          </div>
+          <p>{title}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailDonasi;
