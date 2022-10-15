b5c4d3ea641af62d43a804deb9d5f4082e06cf1b
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-16 03:18:06 +0700

    fix donasi

--- components/Recommendations.tsx
+++ components/Recommendations.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
-
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 type Props = {};
 const data = [
   {
@@ -19,49 +21,62 @@ const dataJenis = [
   {
     id: '1',
     imageUrl: '/image/banjir.jpeg',
-    title: 'Infak',
+    title: 'Gempa Bumi',
   },
   {
     id: '2',
     imageUrl: '/image/banjir.jpeg',
-    title: 'bencana alam',
+    title: 'Angin topan',
+  },
+  {
+    id: '2',
+    imageUrl: '/image/banjir.jpeg',
+    title: 'Longsor',
   },
 ];
 const Recommendations = (props: Props) => {
   const router = useRouter();
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-    <div className="max-w-4xl mx-auto p-2">
-      <h1>Recommendations</h1>
-      <div className="flex space-x-2  overflow-x-scroll lg:overflow-hidden">
-        {data.map((recommendation) => (
-          <div
-            key={recommendation.id}
-            onClick={() =>
-              router.push(
-                {
-                  pathname: `/donasi/${recommendation.id}`,
-                  query: {
-                    imageUrl: recommendation.imageUrl,
-                    title: recommendation.title,
+    <div className="max-w-4xl mx-auto p-2 ">
+      <div className="px-40 ">
+        <Slider {...settings} className=".slick-prev">
+          {data.map((recommendation) => (
+            <div
+              key={recommendation.id}
+              onClick={() =>
+                router.push(
+                  {
+                    pathname: `/donasi/${recommendation.id}`,
+                    query: {
+                      imageUrl: recommendation.imageUrl,
+                      title: recommendation.title,
+                    },
                   },
-                },
-                `/donasi/${recommendation.id}`,
-              )
-            }
-            className="cursor-pointer"
-          >
-            <div className="w-80 h-40 relative rounded-md  overflow-hidden">
-              <Image src={recommendation.imageUrl} layout="fill" alt="" />
+                  `/donasi/${recommendation.id}`,
+                )
+              }
+              className="cursor-pointer"
+            >
+              <p className="text-center">{recommendation.title}</p>
+              <div className="w-80 h-40 relative mx-auto rounded-md flex justify-center items-center ">
+                <Image src={recommendation.imageUrl} layout="fill" alt="" />
+              </div>
             </div>
-            <p>{recommendation.title}</p>
-          </div>
-        ))}
+          ))}
+        </Slider>
       </div>
-      <h1>Jenis Donasi</h1>
-      <div className="flex space-x-2  overflow-x-scroll lg:overflow-hidden">
+      <h1 className="pt-10">Top Penggalangan Dana</h1>
+      <div className="flex space-x-2 justify-between overflow-x-scroll lg:overflow-hidden text-center">
         {dataJenis.map((recommendation) => (
           <div key={recommendation.id} className="cursor-pointer">
-            <div className="w-24 h-24 relative rounded-md overflow-hidden">
+            <div className="w-60 h-24 relative rounded-md overflow-hidden">
               <Image src={recommendation.imageUrl} layout="fill" alt="" />
             </div>
             <p>{recommendation.title}</p>
