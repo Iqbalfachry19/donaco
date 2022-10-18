d96b6213f3e2a819e9b6c14323e92c114e02c201
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-18 22:08:55 +0700

    hi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,17 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Session } from 'next-auth';
 import { withTRPC } from '@trpc/next';
+
 import { ServerRouter } from '../server/router';
+
 import dynamic from 'next/dynamic';
-const SessionProvider = dynamic(() =>
-  import('next-auth/react').then((res) => res.SessionProvider),
-);
+const Layout = dynamic(() => import('../components/layout'));
 const ThirdwebProvider = dynamic(() =>
   import('@thirdweb-dev/react').then((res) => res.ThirdwebProvider),
 );
-const Layout = dynamic(() => import('../components/layout'));
+const SessionProvider = dynamic(() =>
+  import('next-auth/react').then((res) => res.SessionProvider),
+);
 const App = ({
   Component,
   pageProps,
