3e197352e0ef28dd070872a6570c43d43b018a96
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-18 17:27:12 +0700

    hi

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import Head from 'next/head';
 import ButtonDonasi from '../components/ButtonDonasi';
 
 import Hero from '../components/Hero';
-
+export const config = {
+  unstable_runtimeJS: false,
+};
 const Home = () => {
   return (
     <div className="font-body w-full">
