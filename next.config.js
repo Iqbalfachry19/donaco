808c7dfc330bc3372ac7de007687884bf6110fb9
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-18 19:16:57 +0700

    hi

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
 });
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-module.exports = withBundleAnalyzer(nextConfig);
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
+module.exports = nextConfig;
