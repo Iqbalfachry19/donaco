b743fc9415cc13b0e428c9160fff687a40316007
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-18 22:15:40 +0700

    hi

--- components/Recommendations.tsx
+++ components/Recommendations.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 
-import Slider from 'react-slick';
+
+import dynamic from 'next/dynamic';
+const Slider = dynamic(() => import('react-slick'),{ssr:false});
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Link from 'next/link';
