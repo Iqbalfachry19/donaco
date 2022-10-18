8361a5b7d211ed89fea0ba0b7c18b3312e88e953
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-18 19:36:35 +0700

    hi

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,21 +1,12 @@
 import BottomNav from './BottomNav';
-
-import dynamic from 'next/dynamic';
-import { Suspense } from 'react';
-
-const Header = dynamic(() => import('./Header'), {
-  suspense: true,
-});
-
+import Header from './Header';
 type Props = {
   children: React.ReactNode;
 };
 export default function Layout({ children }: Props) {
   return (
     <>
-      <Suspense fallback={`Loading...`}>
-        <Header />
-      </Suspense>
+      <Header />
       <main>{children}</main>
       <BottomNav />
     </>
