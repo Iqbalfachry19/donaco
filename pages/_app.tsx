9f12b2df9681e02feb4780535936fc875c4fa7f9
iqbalfachry19 <ifachry@rocketmail.com>
2022-10-18 16:01:23 +0700

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 import { Session } from 'next-auth';
+import dynamic from 'next/dynamic';
+const ThirdwebProvider = dynamic(() =>
+  import('@thirdweb-dev/react').then((mod) => mod.ThirdwebProvider),
+);
 import { withTRPC } from '@trpc/next';
 import { SessionProvider } from 'next-auth/react';
 import { ServerRouter } from '../server/router';
-import { ThirdwebProvider } from '@thirdweb-dev/react';
+
 const App = ({
   Component,
   pageProps,
@@ -24,7 +28,7 @@ const App = ({
 };
 
 export default withTRPC<ServerRouter>({
-  config({ ctx }) {
+  config() {
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
       : 'http://localhost:3000/api/trpc';
