c3d7b4143b3498d141465aa63a269f2488c014c0
iqbalfachry19 <ifachry@rocketmail.com>
2022-11-06 16:08:47 +0700

    hi

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,20 @@
 import Image from 'next/image';
+import { useTypewriter } from 'react-simple-typewriter';
 
 type Props = {
   title?: string;
 };
 
 const Hero = ({ title }: Props) => {
+  const [text, count] = useTypewriter({
+    words: [
+      'dikerjakan oleh 3 orang developer iqbal, wawan dan naufal',
+      'bantuan dana medis',
+      'bantuan bencana alam',
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
   return (
     <div className="flex flex-col lg:flex-row max-w-4xl py-2 mx-auto items-center">
       <div className="w-80 h-[292px] relative  lg:hidden">
@@ -13,8 +23,8 @@ const Hero = ({ title }: Props) => {
       <div>
         <h1 className="px-2 text-4xl">{title}</h1>
         <p className="text-lg text-bold  px-2">
-          Donaco merupakan aplikasi untuk memberi dana kepada yang membutuhkan
-          dikerjakan oleh 3 orang developer iqbal, wawan dan naufal
+          Donaco merupakan aplikasi untuk memberi dana kepada yang membutuhkan{' '}
+          {text}
         </p>
       </div>
       <div className="w-96 h-52 relative hidden lg:flex">
