38ff9483210b80b2eaab17cafb991e91fa153a1e
iqbalfachry19 <ifachry@rocketmail.com>
2022-11-06 19:42:16 +0700

    hi

--- pages/dashboard/data-donasi.tsx
+++ pages/dashboard/data-donasi.tsx
@@ -4,14 +4,36 @@ import Head from 'next/head';
 
 import { requireAuth } from '../../common/requireAuth';
 import DashboardNav from '../../components/dashboard/DashboardNav';
+import { MediaRenderer, useStorageUpload } from '@thirdweb-dev/react';
+import { useState } from 'react';
 
 type Props = {};
 export const getServerSideProps = requireAuth(async (ctx) => {
   return { props: {} };
 });
-const DataDonasi: NextPage = (props: Props) => {
-  const { data } = useSession();
+const DataDonasi = (props: Props) => {
+  const [selectedImage, setSelectedImage] = useState<any>();
+  const [isImagePicked, setIsImagePicked] = useState(false);
+  const [uri, setUri] = useState<any>();
+  const { mutateAsync: upload } = useStorageUpload();
+  async function uploadData() {
+    // Get any data that you want to upload
+    const reader: any = new window.FileReader();
+    reader?.readAsArrayBuffer(selectedImage);
+    reader.onloadend = async () => {
+      const buffer = Buffer.from(reader.result);
+      const dataToUpload = [buffer];
 
+      // And upload the data with the upload function
+      const uris = await upload({ data: dataToUpload });
+      console.log(uris);
+      setUri(uris);
+    };
+  }
+  const changeImageHandler = (event: any) => {
+    setSelectedImage(event.target.files[0]);
+    setIsImagePicked(true);
+  };
   return (
     <div className="grid grid-cols-5 h-screen">
       <Head>
@@ -31,16 +53,19 @@ const DataDonasi: NextPage = (props: Props) => {
             otherwise you would be redirected to the login page.
           </p>
           <div className="my-4 bg-gray-700 rounded-lg p-4 text-white">
-            <pre>
-              <code>{JSON.stringify(data, null, 2)}</code>
-            </pre>
+            <input
+              type="file"
+              name="file"
+              accept="image/*"
+              required
+              onChange={changeImageHandler}
+              className="text-black"
+            />
+            <MediaRenderer src={uri[0]} alt="A mp4 video" />
           </div>
           <div className="text-center">
-            <button
-              className="btn btn-secondary"
-              onClick={() => signOut({ callbackUrl: '/' })}
-            >
-              Logout
+            <button className="btn btn-secondary" onClick={uploadData}>
+              Upload
             </button>
           </div>
         </div>
