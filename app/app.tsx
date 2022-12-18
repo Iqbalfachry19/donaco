08197568068152e270fc207f18f865989b5dc3ae
iqbalfachry19 <ifachry@rocketmail.com>
2022-12-18 09:19:31 +0700

    hi

--- app/app.tsx
+++ app/app.tsx
@@ -2,14 +2,9 @@
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 import { SessionProvider } from 'next-auth/react';
 
-export default function App({
-  children,
-  ...props
-}: {
-  children: React.ReactNode;
-}) {
+export default function App({ children, session }: any) {
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
         {children}
       </ThirdwebProvider>
